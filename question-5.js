eacde31430d9c72a63ca11adc4551185ca69a391
Patchara <sombrousnorn@gmail.com>
2025-09-15 10:04:47 +0700

    feat(question-5):ans-q5

--- question-5.js
+++ question-5.js
@@ -1,6 +1,6 @@
 /*  
 What is the Big O notation for Function sortProductsByPrice of the Question 4. Explain your reason.
-Answer:
+Answer: Big O = O(n^2) เพราะ การbubble sort เป็นการวนloopเข้าหาข้อมูลทุกตัว และต้องสลับที่ข้อมูล อาจจะทำให้จำนวนรอบ ยกกำลังมาก ตามจำนวนข้อมูล
 
 
 */
